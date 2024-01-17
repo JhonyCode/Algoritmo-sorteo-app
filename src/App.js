588d598152daab137179800693c2e58e29e24dfc
JhonyCode <jhonygane@gmail.com>
2024-01-17 19:04:00 +0100

    Starting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container-fluid d-flex flex-column vh-100 justify-content-center align-items-center">
+        {/* Header */}
+        <header className="mb-4">
+          <h1>My React Bootstrap App</h1>
+        </header>
+
+        <div className="text-center">
+          <h2>Bienvenido a mi aplicación</h2>
+          <button className="btn btn-primary mt-3">Haz clic</button>
+        </div>
+      </div>
   );
 }
 
